314af473181f95bf8cc539a905c5408da6243f16
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-04-15 19:18:31 +0700

    Welcome Section. Done!

--- Decryptor.py
+++ Decryptor.py
@@ -1,10 +1,20 @@
 #Title
 import pyfiglet
 from colorama import Fore, Back, Style
-
 print(Fore.YELLOW + pyfiglet.figlet_format(" Decrypt It!", font = "roman", width = 1000))
 
 #Greeting
+from pyboxen import boxen
+print(
+    boxen(
+        "This program will help you to convert an encrypted message back to its origial and readable format.",
+            title="WELCOME TO DECRYPT IT!",
+            title_alignment="center",
+            color="bold magenta",
+            padding=1,
+    )
+)
+
 #Ask the user for the encrypted text he/she wants to decrypt
 #Decrypt the text by replacing/changing the following characters with their corresponding alphabet
 #Processing the Output/Loading
