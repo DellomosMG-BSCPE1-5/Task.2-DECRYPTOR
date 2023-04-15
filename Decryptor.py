cd4712239dc1a1920674ad3aa15868f823b6226d
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-04-15 19:05:56 +0700

    Title/Header Section. Done!

--- Decryptor.py
+++ Decryptor.py
@@ -1,4 +1,9 @@
 #Title
+import pyfiglet
+from colorama import Fore, Back, Style
+
+print(Fore.YELLOW + pyfiglet.figlet_format(" Decrypt It!", font = "roman", width = 1000))
+
 #Greeting
 #Ask the user for the encrypted text he/she wants to decrypt
 #Decrypt the text by replacing/changing the following characters with their corresponding alphabet
