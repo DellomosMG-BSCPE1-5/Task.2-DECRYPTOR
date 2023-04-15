7e07bde941ae1364aca347e90fb73bba4bb89934
MG Dellomos <dellomosmarygrace0402@gmail.com>
2023-04-15 19:01:05 +0700

    The Pseudocode

---
+++ Decryptor.py
@@ -0,0 +1,7 @@
+#Title
+#Greeting
+#Ask the user for the encrypted text he/she wants to decrypt
+#Decrypt the text by replacing/changing the following characters with their corresponding alphabet
+#Processing the Output/Loading
+#Display the decrypted text to the user
+#Ask the user if he/she wants to try again the program.
